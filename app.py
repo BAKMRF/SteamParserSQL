61c8ba7c280542a7af1129f91fe2546770e2b816
BAKMRF <fragresgg@gmail.com>
2026-04-21 15:03:12 +0300

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-# Настройки Postgre
+# Настройки Post
 DB_CONFIG = {
     'host': os.getenv('DB_HOST', '127.0.0.1'),
     'port': os.getenv('DB_PORT', '5433'),
